b601741bc1de0d0d0af8e91f34a1ad2baf8eb5f2
Turan Babasoy <babasoyturan1@gmail.com>
2026-07-11 17:35:03 +0400

    test: use writable storage path in document web tests

--- backend/document-service/src/test/java/oopsops/app/document/DocumentControllerTest.java
+++ backend/document-service/src/test/java/oopsops/app/document/DocumentControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(DocumentController.class)
+@ActiveProfiles("test")
 public class DocumentControllerTest {
 
     @Autowired
